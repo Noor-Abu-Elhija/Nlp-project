e0521556595e0e11c91f8821e2a840e1330a50a3
kinan-02 <49200338+kinan-02@users.noreply.github.com>
2024-06-07 15:38:20 +0300

    Add files via upload

---
+++ RunningScripts/final_sweep_kinan-02.py
@@ -0,0 +1,30 @@
+import wandb
+YOUR_WANDB_USERNAME = "kinan-i-2002"
+project = "NLP2024_PROJECT_kinan-02"
+
+command = [
+        "${ENVIRONMENT_VARIABLE}",
+        "${interpreter}",
+        "StrategyTransfer.py",
+        "${project}",
+        "${args}"
+    ]
+
+sweep_config = {
+    "name": "Final Model",
+    "method": "grid",
+    "metric": {
+        "goal": "maximize",
+        "name": "AUC.test.max"
+    },
+    "parameters": {
+        "seed": {"values": list(range(1, 10))},
+        "ENV_LEARNING_RATE": {"values": [0.001, 0.0001, 1e-05, 4e-05, 0.0004]}
+    },
+    "command": command
+}
+
+# Initialize a new sweep
+sweep_id = wandb.sweep(sweep=sweep_config, project=project)
+print("run this line to run your agent in a screen:")
+print(f"screen -dmS \"sweep_agent\" wandb agent {YOUR_WANDB_USERNAME}/{project}/{sweep_id}")
